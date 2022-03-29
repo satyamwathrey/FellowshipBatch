ae1438d83b05eb45619a0d706a92fc8991cdd5ab
satyamwathrey <swathrey1@gmail.com>
2022-03-29 23:43:02 +0530

    Added employee attendance

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+
+            if (isPresent == empInput)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadLine();
+        }
+    }
+}
